af5eb301cf869375496022f9e44760373faed411
sebassil12 <anddysilva15@gmail.com>
2025-02-23 18:03:54 -0500

    ADD: Dialog Box And download path

--- download.py
+++ download.py
@@ -3,17 +3,13 @@ import os
 class Download:
     def __init__(self, output_path=None):
         if output_path is None:
-            potential_path = os.path.join(os.path.expanduser('~'), 'Downloads')
-            if os.path.exists(potential_path):
-                self.output_path = potential_path
-            else:
-                self.output_path = './'
+            output_path = os.path.join(os.path.expanduser('~'), 'Downloads')
         self.output_path = output_path
-        
-    def download_audio(self, url) :
+       
+    def download_audio(self, url):
         ydl_opts = {
             'format': 'bestaudio/best',
-            'outtmpl': f'{self.output_path}/%(title)s.%(ext)s',  
+            'outtmpl': os.path.join(self.output_path, '%(title)s.%(ext)s'),  
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
@@ -23,8 +19,3 @@ class Download:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
 
-if __name__ == '__main__':
-    url = input("Introduce la URL del video de YouTube: ")
-    downloader = Download()
-    downloader.download_audio(url)
-    print("Descarga completada.")
--- interface.py
+++ interface.py
@@ -1,45 +1,36 @@
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QLabel, QLineEdit, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, \
+                            QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, \
+                            QWidget, QDialog, QDialogButtonBox, QGridLayout
 import sys
 from download import Download
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        self.setWindowTitle("Download YOutube music")
+        self.setWindowTitle("Download Youtube music")
         self.setGeometry(100, 100, 800, 600)
 
         self.label = QLabel()
+        self.label_path_download = QLabel()
         self.label.setText("Introduce la URL del video de YouTube:")
+        self.label_path_download.setText("Introduce la ruta donde se guardan tus descargas:")
         self.input = QLineEdit()
+        self.input_path_download = QLineEdit()
 
         self.button = QPushButton("Download")
         self.button.clicked.connect(self.download_audio)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.input)
-        layout.addWidget(self.button)
-        
+        layout = QGridLayout()
+        layout.addWidget(self.label_path_download, 0, 0)
+        layout.addWidget(self.input_path_download, 0, 1)
+        layout.addWidget(self.label, 1, 0)
+        layout.addWidget(self.input, 1, 1)
+        layout.addWidget(self.button, 2, 0)
         container = QWidget()
         container.setLayout(layout)
 
         self.setCentralWidget(container)
 
         self.setStyleSheet("""
-            QMainWindow {
-                background-color: #f0f0f0;
-                display: flex;
-            }
-            QLabel {
-                font-size: 16px;
-                color: #333;
-            }
-            QLineEdit {
-                font-size: 14px;
-                padding: 5px;
-                border: 1px solid #ccc;
-                border-radius: 5px;
-            }
             QPushButton {
                 font-size: 14px;
                 background-color: #4CAF50;
@@ -54,10 +45,36 @@ class MainWindow(QMainWindow):
         """)
 
     def download_audio(self):
-        url = self.input.text()
-        downloader = Download()
-        downloader.download_audio(url)
-        print("Descarga completada.")
+        dlg = QDialog(self)
+        output_path = self.input_path_download.text()
+        try:
+            url = self.input.text()
+            downloader = Download(output_path)
+            downloader.download_audio(url)
+            dlg = CustomDialog("Descarga completada.", self)
+            
+        except Exception as e:
+            dlg = CustomDialog(f"Error: {str(e)}", self)
+
+        dlg.exec()
+class CustomDialog(QDialog):
+    def __init__(self, text_message, *args, **kwargs):
+        super(CustomDialog, self).__init__(*args, **kwargs) 
+
+        self.setWindowTitle("Important!")
+
+        QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        layout = QVBoxLayout()
+        message = QLabel(text_message)
+        layout.addWidget(message)
+        layout.addWidget(self.buttonBox)
+        self.setLayout(layout)
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
